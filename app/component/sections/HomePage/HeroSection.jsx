ef14c50869cc15456b01ffd65ced395014e149c1
Sanket0798 <sanketrathod1398@gmail.com>
2025-02-20 16:31:35 +0530

    Minor changes in configuration files.

--- app/component/sections/HomePage/HeroSection.jsx
+++ app/component/sections/HomePage/HeroSection.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import Image from "next/image";
-import "../../../styles/InvertedRadius.css";
+// import "../../../styles/InvertedRadius.css";
 
 const HeroSection = () => {
   return (
-    <div class="house-info">
-      <div className="house-image">
-        <img
-          src="https://images.unsplash.com/photo-1531971589569-0d9370cbe1e5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODE3NTgzNDU&ixlib=rb-4.0.3&q=80&w=400"
-          alt="House exterior"
-        />
-      </div>
+    // <div class="house-info">
+    //   <div className="house-image">
+    //     <img
+    //       src="https://images.unsplash.com/photo-1531971589569-0d9370cbe1e5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2ODE3NTgzNDU&ixlib=rb-4.0.3&q=80&w=400"
+    //       alt="House exterior"
+    //     />
+    //   </div>
 
-      <div class="house-price">
-        <span> $500,000</span>
-      </div>
+    //   <div class="house-price">
+    //     <span> $500,000</span>
+    //   </div>
 
-      <ul class="house-meta">
-        <li>Somewhere</li>
-        <li>
-          158m<sup>2</sup>
-        </li>
-        <li>Big house</li>
-      </ul>
-    </div>
-    // <p>HeroSection</p>
+    //   <ul class="house-meta">
+    //     <li>Somewhere</li>
+    //     <li>
+    //       158m<sup>2</sup>
+    //     </li>
+    //     <li>Big house</li>
+    //   </ul>
+    // </div>
+    <p>HeroSection</p>
   );
 };
 
