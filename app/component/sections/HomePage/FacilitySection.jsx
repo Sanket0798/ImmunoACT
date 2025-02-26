fa939090e775abb670cdb0b9db6fb134f595fc66
Sanket0798 <sanketrathod1398@gmail.com>
2025-02-26 13:13:52 +0530

    FacilitySection has been completed with responsive design.

--- app/component/sections/HomePage/FacilitySection.jsx
+++ app/component/sections/HomePage/FacilitySection.jsx
@@ -1,82 +1,66 @@
-import React from "react";
+import React, { memo } from "react";
 import HeroVideoDialog from "../../../../components/magicui/hero-video-dialog";
 import { motion } from "framer-motion";
-import {
-  CodeContainer,
-  StaticStep,
-} from "../../../../components/eldoraui/staticstepper";
+import { StaticStep } from "../../../../components/eldoraui/staticstepper";
+import { dataSteps } from "@/app/data/sharedData";
 
-const dataSteps = [
-  {
-    title: "End to end cell and gene therapy solutions",
-    code: "npx create-react-app my-app",
-  },
-  {
-    title: "Expertise in novel therapeutic design and plasmid DNA production",
-    code: "cd my-app",
-  },
-  {
-    title:
-      "Expertise in GMP grade viral vectors manufacturing & advanced ex-VIVO genetic engineering",
-    code: "npm start",
-  },
-  {
-    title:
-      "Maintaining global standards of cell therapy products with a dedicated quality control facility",
-    code: "npm run build",
-  },
-  {
-    title:
-      "Trained teams for clinical trials, regulatory compliances, Marketing and customer support",
-  },
-];
-
-const FacilitySection = () => {
-  return (
-    <section className="container mx-auto py-10">
+const FacilitySection = memo(() => (
+  <section className="container mx-auto py-10 md:py-0 lg:py-10 px-4 sm:px-6 lg:px-8">
+    {/* FacilitySection Tag */}
+    <motion.div
+      initial={{ opacity: 0, scale: 0.8 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.6, delay: 0.1, ease: "easeOut" }}
+      className="flex items-center gap-3 sm:gap-4 mb-3"
+    >
       <motion.div
         initial={{ width: 0 }}
-        whileInView={{ width: "auto" }}
-        transition={{ duration: 0.8, delay: 0.2 }}
-        className="flex items-center gap-3 sm:gap-4"
+        whileInView={{ width: "3rem" }}
+        transition={{ duration: 0.5, delay: 0.3, ease: "easeOut" }}
+        className="h-[1px] bg-black"
+      ></motion.div>
+      <motion.span
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.5, ease: "easeOut" }}
+        className="text-2xl md:text-4xl font-bold text-[#232e24] sm:text-4xl font-outfit"
       >
-        <motion.div
-          initial={{ width: 0 }}
-          whileInView={{ width: "3rem" }}
-          transition={{ duration: 0.4, delay: 0.4 }}
-          className="h-[1px] bg-black"
-        ></motion.div>
-        <motion.span
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ duration: 0.4, delay: 0.6 }}
-          className="text-3xl font-bold text-[#232e24] sm:text-4xl font-outfit"
-        >
-          Our Manufacturing Facility
-        </motion.span>
-      </motion.div>
+        Manufacturing Facility
+      </motion.span>
+    </motion.div>
 
-      <div className="flex flex-col md:flex-row justify-between items-start gap-8">
-        <div className="w-full max-w-2xl p-4">
-          {dataSteps.map((step, index) => (
-            <StaticStep key={step.title} title={step.title}>
-              {/* <CodeContainer>{step.code}</CodeContainer> */}
-            </StaticStep>
-          ))}
-        </div>
+    {/* FacilitySection Heading */}
+    <motion.span
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 0.4, delay: 0.8 }}
+      className="text-lg font-medium text-[#232e24] font-outfit"
+    >
+      ImmunoACT's new state-of-the-art GMP Facility that can cater to the best
+      quality cell and gene therapy products and treat up to 1500 patients per
+      year.
+    </motion.span>
 
-        <div className="relative flex justify-center">
-          <HeroVideoDialog
-            className="block dark:hidden"
-            animationStyle="from-center"
-            videoSrc="https://www.youtube.com/embed/AwroDnmW-Ds?si=orhVhi6bUqdqfb0P"
-            thumbnailSrc="/assets/sections/FacilitySection/FacilityThumbnail.jpg"
-            thumbnailAlt="Hero Video"
-          />
-        </div>
+    {/* Stepper Points */}
+    <div className="flex flex-col md:flex-row justify-between items-center gap-8 mt-4">
+      <div className="md:w-1/2 lg:w-1/2 px-4 md:p-4">
+        {dataSteps.map(({ title, code }) => (
+          <StaticStep key={title} title={title}></StaticStep>
+        ))}
       </div>
-    </section>
-  );
-};
+
+      {/* Video Card */}
+      <div className="relative flex justify-center w-full md:w-1/2 lg:w-1/2">
+        <HeroVideoDialog
+          className="block dark:hidden w-full h-auto"
+          animationStyle="from-center"
+          videoSrc="https://www.youtube.com/embed/AwroDnmW-Ds?si=orhVhi6bUqdqfb0P"
+          thumbnailSrc="/assets/sections/FacilitySection/FacilityThumbnail.jpg"
+          thumbnailAlt="Hero Video"
+        />
+      </div>
+    </div>
+  </section>
+));
 
 export default FacilitySection;
--- components/eldoraui/staticstepper.tsx
+++ components/eldoraui/staticstepper.tsx
@@ -12,13 +12,13 @@ export const StaticStep = ({
   return (
     <div className="flex gap-6">
       <div className="flex flex-col items-center">
-        <p className="flex size-8 flex-none select-none items-center justify-center rounded-full border border-neutral-400/20 bg-neutral-100 text-sm font-medium text-neutral-700 dark:border-neutral-400/10 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80">
+        <p className="flex size-8 flex-none select-none items-center justify-center  border border-neutral-400/20 text-sm font-medium text-neutral-700 dark:border-neutral-400/10 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80">
           •
         </p>
         <div className="relative my-2 h-full w-px rounded-full bg-black" />
       </div>
       <div className="mb-4 w-full">
-        <h6 className="mb-4 text-sm font-medium tracking-tight text-neutral-700 dark:text-neutral-50">
+        <h6 className="my-2 text-sm font-medium tracking-tight text-neutral-700 dark:text-neutral-50">
           {title}
         </h6>
         {children}
